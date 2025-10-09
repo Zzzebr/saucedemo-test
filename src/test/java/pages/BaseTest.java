753a356a4c483b9780b8bc0aa7b95d76f9433f2d
Kluchnik-N <ilya.vasilevv@bk.ru>
2025-10-09 09:39:19 +0300

    Initial commit

---
+++ src/test/java/pages/BaseTest.java
@@ -0,0 +1,38 @@
+package pages;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
+import java.time.Duration;
+
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+// options.addArguments("--headless=new"); // раскомментируйте, если нужен headless
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0)); // используем явные ожидания
+    }
+
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
